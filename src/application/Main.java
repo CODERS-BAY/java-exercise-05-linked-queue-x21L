43fd206897e097e49890cbddfff69d433ed0acc1
Lukas Wais <lukas.wais@outlook.de>
2021-12-03 19:12:58 +0100

    Abgabe

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,9 +3,7 @@ package application;
 public class Main {
 
 	public static void main(String[] args) {
-		/*
-		 * some testing here
-		 */
+		System.out.println("Assume I am ready to submit my code :-)");
 	}
 
 }
